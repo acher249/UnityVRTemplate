ab8a1741b7ddf6bcaf5e58f0ad9d4428e8ca4637
acher249 <32597382+acher249@users.noreply.github.com>
2018-08-08 15:29:17 -0400

    Asset Bundle Export
    Asset Bundle Export.
    
    Broke VR and First Person.. Need to jump back.

--- HOK_UnityTemplate_VR/Assets/Editor/SaveAssetBundles.cs
+++ HOK_UnityTemplate_VR/Assets/Editor/SaveAssetBundles.cs
@@ -18,6 +18,8 @@ public class CreateAssetBundles
     [MenuItem("Assets/Build Bundle Android")]
     static void BuildAllAssetBundlesAndroid()
     {
+        // Get some public function from another script to scale the model down automatically before
+        // the asset bundle is built. So you dont have to do it manually.
         string assetBundleDirectory = "Assets/AssetBundles";
         if (!Directory.Exists(assetBundleDirectory))
         {
