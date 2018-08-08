146ea0784eb4d4ea981a87a1bf2c85e28b079a30
acher249 <32597382+acher249@users.noreply.github.com>
2018-08-08 11:14:19 -0400

    Android AssetBundles
    Android AssetBundles

--- HOK_UnityTemplate_VR/Assets/Editor/SaveAssetBundles.cs
+++ HOK_UnityTemplate_VR/Assets/Editor/SaveAssetBundles.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 public class CreateAssetBundles
 {
-    [MenuItem("Assets/Build AssetBundles")]
+    [MenuItem("Assets/Build Bundle Windows")]
     static void BuildAllAssetBundles()
     {
         string assetBundleDirectory = "Assets/AssetBundles";
@@ -13,5 +13,17 @@ public class CreateAssetBundles
         }
         // Choose what platform to build the bundle out to
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Assets/Build Bundle Android")]
+    static void BuildAllAssetBundlesAndroid()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        // Choose what platform to build the bundle out to
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
     }
 }
